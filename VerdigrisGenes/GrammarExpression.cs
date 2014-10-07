712f89d93cc1d5f9512be8d2d695f11c8de7230a
John Colagioia <john@colagioia.net>
2014-10-07 07:17:05 -0400

    Style

--- VerdigrisGenes/GrammarExpression.cs
+++ VerdigrisGenes/GrammarExpression.cs
@@ -1,15 +1,34 @@
-﻿using System;
-
+﻿// <copyright file="GrammarExpression.cs" company="John Colagioia">
+//     John.Colagioia.net. Licensed under the GPLv3
+// </copyright>
+// <author>John Colagioia</author>
 namespace VerdigrisGenes
 {
+        using System;
+
         public class GrammarExpression
         {
+                /// <summary>
+                /// The expression.
+                /// </summary>
                 private readonly string expression;
 
+                /// <summary>
+                /// Whether the expression will require an initialized variable.
+                /// </summary>
                 private readonly bool reqValue;
 
+                /// <summary>
+                /// Whether the expression will initialize a variable.
+                /// </summary>
                 private readonly bool initialize;
 
+                /// <summary>
+                /// Initializes a new instance of the <see cref="VerdigrisGenes.GrammarExpression"/> class.
+                /// </summary>
+                /// <param name="expr">Expr.</param>
+                /// <param name="needValue">If set to <c>true</c> need value.</param>
+                /// <param name="init">If set to <c>true</c> init.</param>
                 public GrammarExpression(string expr, bool needValue, bool init)
                 {
                         this.expression = expr;
@@ -17,29 +36,40 @@ namespace VerdigrisGenes
                         this.initialize = init;
                 }
 
+                /// <summary>
+                /// Gets the expression.
+                /// </summary>
+                /// <value>The expression.</value>
                 public string Expression
                 {
                         get
                         {
-                                return expression;
+                                return this.expression;
                         }
                 }
 
+                /// <summary>
+                /// Gets a value indicating whether this <see cref="VerdigrisGenes.GrammarExpression"/> req value.
+                /// </summary>
+                /// <value><c>true</c> if req value; otherwise, <c>false</c>.</value>
                 public bool ReqValue
                 {
                         get
                         {
-                                return reqValue;
+                                return this.reqValue;
                         }
                 }
 
+                /// <summary>
+                /// Gets a value indicating whether this <see cref="VerdigrisGenes.GrammarExpression"/> is initialize.
+                /// </summary>
+                /// <value><c>true</c> if initialize; otherwise, <c>false</c>.</value>
                 public bool Initialize
                 {
                         get
                         {
-                                return initialize;
+                                return this.initialize;
                         }
                 }
         }
 }
-
--- VerdigrisGenes/Variable.cs
+++ VerdigrisGenes/Variable.cs
@@ -1,20 +1,41 @@
-﻿using System;
-
+﻿// <copyright file="Variable.cs" company="John Colagioia">
+//     John.Colagioia.net. Licensed under the GPLv3
+// </copyright>
+// <author>John Colagioia</author>
 namespace VerdigrisGenes
 {
+        using System;
+
         public class Variable
         {
+                /// <summary>
+                /// The variable name.
+                /// </summary>
                 private readonly string name;
 
+                /// <summary>
+                /// Has the variable been initialized?
+                /// </summary>
                 private bool initialized = false;
 
+                /// <summary>
+                /// Are we running a statement that will initialize the variable?
+                /// </summary>
                 private bool toInitialize = false;
 
+                /// <summary>
+                /// Initializes a new instance of the <see cref="VerdigrisGenes.Variable"/> class.
+                /// </summary>
+                /// <param name="name">Name.</param>
                 public Variable(string name)
                 {
                         this.name = name;
                 }
 
+                /// <summary>
+                /// Gets the variable's name.
+                /// </summary>
+                /// <value>The name.</value>
                 public string Name
                 {
                         get
@@ -23,6 +44,10 @@ namespace VerdigrisGenes
                         }
                 }
 
+                /// <summary>
+                /// Gets or sets a value indicating whether this <see cref="VerdigrisGenes.Variable"/> is initialized.
+                /// </summary>
+                /// <value><c>true</c> if initialized; otherwise, <c>false</c>.</value>
                 public bool Initialized
                 {
                         get
@@ -36,14 +61,21 @@ namespace VerdigrisGenes
                         }
                 }
 
+                /// <summary>
+                /// Sets a value indicating whether this <see cref="VerdigrisGenes.Variable"/> will be initialized.
+                /// </summary>
+                /// <value><c>true</c> if to initialize; otherwise, <c>false</c>.</value>
                 public bool ToInitialize
                 {
                         set
                         {
-                                toInitialize = value;
+                                this.toInitialize = value;
                         }
                 }
 
+                /// <summary>
+                /// Update this instance.
+                /// </summary>
                 public void Update()
                 {
                         if (this.toInitialize)
@@ -54,4 +86,3 @@ namespace VerdigrisGenes
                 }
         }
 }
-
