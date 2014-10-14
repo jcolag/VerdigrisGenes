56eb7700b2d96a8f8677503baf18dc7432f9ee19
John Colagioia <john@colagioia.net>
2014-10-14 07:13:59 -0400

    Framework for interpreter

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="Interpreter.cs" company="John Colagioia">
+//     John.Colagioia.net. Licensed under the GPLv3
+// </copyright>
+// <author>John Colagioia</author>
+namespace Interpreter
+{
+        using System;
+        using System.Collections.Generic;
+
+        public class Interpreter
+        {
+                private List<Statement> program;
+
+                public Interpreter()
+                {
+                        program = new List<Statement>();
+                }
+
+                public bool Parse(string program)
+                {
+                        var nesting = new Stack<Statement>();
+                        string[] nl = { Environment.NewLine };
+                        string[] lines = program.Split(nl, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string line in lines)
+                        {
+                                var s = new Statement();
+                                bool nest = s.Parse(line);
+                                if (nesting.Count == 0)
+                                {
+                                        this.program.Add(s);
+                                }
+                                else
+                                {
+                                        Statement ctrl = nesting.Peek();
+                                        ctrl.Nest(s);
+                                }
+
+                                if (nest)
+                                {
+                                        nesting.Push(s);
+                                }
+                                else if (s.IsEnd)
+                                {
+                                        if (s.IsLoop == nesting.Peek().IsLoop)
+                                        {
+                                                nesting.Pop();
+                                        }
+                                        else
+                                        {
+                                                return false;
+                                        }
+                                }
+                        }
+
+                        return true;
+                }
+
+                public bool Go()
+                {
+                        return true;
+                }
+        }
+}
+
---
+++ Interpreter/Statement.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="Interpreter.cs" company="John Colagioia">
+//     John.Colagioia.net. Licensed under the GPLv3
+// </copyright>
+// <author>John Colagioia</author>
+namespace Interpreter
+{
+        using System;
+        using System.Collections.Generic;
+
+        public class Statement
+        {
+                private List<Statement> nest;
+
+                private bool isNestable = false;
+
+                private bool isLoop = false;
+
+                private bool isEnd = false;
+
+                public Statement()
+                {
+                        this.nest = new List<Statement>();
+                }
+
+                public Statement(string line)
+                {
+                        this.nest = new List<Statement>();
+                        this.Parse(line);
+                }
+
+                public bool IsLoop
+                {
+                        get
+                        {
+                                return this.isLoop;
+                        }
+                }
+
+                public bool IsEnd
+                {
+                        get
+                        {
+                                return this.isEnd;
+                        }
+                }
+
+                public bool Parse(string line)
+                {
+                        string[] nl = { Environment.NewLine };
+                        string[] tokens = line.ToLower().Split(nl, StringSplitOptions.RemoveEmptyEntries);
+                        bool block = false;
+
+                        switch(tokens[0])
+                        {
+                        case "define":
+                                break;
+                        case "input":
+                                break;
+                        case "output":
+                                break;
+                        case "let":
+                                break;
+                        case "while":
+                                block = true;
+                                this.isLoop = true;
+                                break;
+                        case "if":
+                                block = true;
+                                break;
+                        case "end":
+                                this.isEnd = true;
+                                break;
+                        case "#":
+                                break;
+                        }
+
+                        this.isNestable = block;
+                        return this.isNestable;
+                }
+
+                public void Nest(Statement item)
+                {
+                        this.nest.Add(item);
+                }
+        }
+}
+
