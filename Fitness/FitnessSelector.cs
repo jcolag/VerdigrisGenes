97ef67544c5d5ad0a9e22090bae405b890c906b7
John Colagioia <john@colagioia.net>
2014-10-24 13:49:40 -0400

    Styling for fitness evaluator

--- Fitness/FitnessSelector.cs
+++ Fitness/FitnessSelector.cs
@@ -6,23 +6,51 @@ namespace Fitness
 {
         using System;
         using System.Collections.Generic;
-        using VerdigrisGenes;
         using Interpreter;
+        using VerdigrisGenes;
 
+        /// <summary>
+        /// Fitness selector.
+        /// </summary>
         public class FitnessSelector
         {
+                /// <summary>
+                /// The expected outputs.
+                /// </summary>
                 private readonly List<int> outputs;
 
+                /// <summary>
+                /// The program genome.
+                /// </summary>
                 private readonly Verdigris genome;
 
+                /// <summary>
+                /// The interpreter.
+                /// </summary>
                 private readonly Interpreter terp;
 
+                /// <summary>
+                /// The program.
+                /// </summary>
                 private readonly string program;
 
+                /// <summary>
+                /// Whether the program has been executed.
+                /// </summary>
                 private bool executed;
 
+                /// <summary>
+                /// The program's fitness rating.
+                /// </summary>
                 private double rating = 0.0;
 
+                /// <summary>
+                /// Initializes a new instance of the <see cref="Fitness.FitnessSelector"/> class.
+                /// </summary>
+                /// <param name="grammar">The program grammar.</param>
+                /// <param name="initialGenome">Initial genome.</param>
+                /// <param name="inputs">Standard inputs.</param>
+                /// <param name="outputs">Expected outputs.</param>
                 public FitnessSelector(string grammar, string initialGenome, List<int> inputs, List<int> outputs)
                 {
                         string nl = Environment.NewLine;
@@ -38,11 +66,15 @@ namespace Fitness
 
                         this.program = this.genome.GenerateProgram("Program");
                         this.program = this.program.Replace(" ;", nl).Replace(nl + " ", nl);
-                        this.terp.Parse(program);
+                        this.terp.Parse(this.program);
 
                         this.executed = false;
                 }
 
+                /// <summary>
+                /// Gets the program's fitness rating.
+                /// </summary>
+                /// <value>The rating.</value>
                 public double Rating
                 {
                         get
@@ -51,11 +83,19 @@ namespace Fitness
                         }
                 }
 
+                /// <summary>
+                /// Execute this instance's program.
+                /// </summary>
                 public void Execute()
                 {
                         this.executed = this.terp.Go();
                 }
 
+                /// <summary>
+                /// Evaluate the program against the specified threshold.
+                /// </summary>
+                /// <param name="threshold">Threshold for success.</param>
+                /// <returns>True if the program exceeds the threshold, otherwise false.</returns>
                 public bool Evaluate(double threshold)
                 {
                         var result = new List<int>(this.terp.Outputs);
@@ -81,6 +121,13 @@ namespace Fitness
                         return this.rating >= threshold;
                 }
 
+                /// <summary>
+                /// Evaluate the specified result, expected and score.
+                /// </summary>
+                /// <param name="result">Result value.</param>
+                /// <param name="expected">Expected value.</param>
+                /// <param name="score">Score to date.</param>
+                /// <returns>Current score of coincidence with expected results.</returns>
                 private static double Evaluate(int result, int expected, double score)
                 {
                         double diff = Math.Abs((double)result - (double)expected);
@@ -95,6 +142,13 @@ namespace Fitness
                         return score * component;
                 }
 
+                /// <summary>
+                /// Pad the specified list to max length with item.
+                /// </summary>
+                /// <param name="list">List to pad.</param>
+                /// <param name="max">Maximum list length.</param>
+                /// <param name="item">Item for padding.</param>
+                /// <returns>Final (padded) length.</returns>
                 private static int Pad(ICollection<int> list, int max, int item)
                 {
                         int count = 0;
--- Fitness/Properties/AssemblyInfo.cs
+++ Fitness/Properties/AssemblyInfo.cs
@@ -1,9 +1,12 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="John Colagioia">
+//     John.Colagioia.net. Licensed under the GPLv3
+// </copyright>
+// <author>John Colagioia</author>
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-
 [assembly: AssemblyTitle("Fitness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -16,12 +19,10 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
 [assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+// [assembly: AssemblyDelaySign(false)]
+// [assembly: AssemblyKeyFile("")]
