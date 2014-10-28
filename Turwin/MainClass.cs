f057bfed2cc7f8f550e2c39adf9a9b95f98ef4d9
John Colagioia <john@colagioia.net>
2014-10-28 07:42:15 -0400

    Quick outline of generation process

--- Turwin/MainClass.cs
+++ Turwin/MainClass.cs
@@ -15,6 +15,11 @@ namespace Turwin
         public class MainClass
         {
                 /// <summary>
+                /// The population size.
+                /// </summary>
+                private const int population = 100;
+
+                /// <summary>
                 /// The entry point of the program, where the program control starts and ends.
                 /// </summary>
                 /// <param name="args">The command-line arguments.</param>
@@ -22,9 +27,10 @@ namespace Turwin
                 {
                         var stdin = new List<int>() { 720 };
                         var stdout = new List<int>() { 2, 2, 2, 2, 3, 3, 5 };
-                        FitnessSelector fit;
+                        var fit = new List<FitnessSelector>();
                         string nl = Environment.NewLine;
                         string genometext = string.Empty;
+                        double threshold = 0.0;
 
                         if (args.Length == 0)
                         {
@@ -43,10 +49,26 @@ namespace Turwin
                                 input.Close();
                         }
 
-                        fit = new FitnessSelector(grammartext, genometext, stdin, stdout);
-                        fit.Execute();
-                        bool accept = fit.Evaluate(90.0);
-                        Console.WriteLine(accept.ToString() + ": " + fit.Rating.ToString() + "%");
+                        for (int i = 0; i < MainClass.population; i++)
+                        {
+                                var f = new FitnessSelector(grammartext, genometext, stdin, stdout);
+                                genometext = string.Empty;
+                                f.Execute();
+                                f.Evaluate(threshold);
+                                fit.Add(f);
+                                bool accept = f.Rating >= threshold;
+                                Console.WriteLine(i.ToString() + " - " + accept.ToString() + ": " + f.Rating.ToString() + "%");
+                        }
+
+                        fit.Sort();
+                        for (int i = 0; i < fit.Count; i++)
+                        {
+                                if (fit[i].Rating < threshold)
+                                {
+                                        fit.RemoveRange(i, fit.Count - i);
+                                        break;
+                                }
+                        }
                 }
         }
 }
