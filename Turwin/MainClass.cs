800774ddb440e1cd1b403b6ed91e1f636eefb4e6
John Colagioia <john@colagioia.net>
2014-11-03 06:11:07 -0500

    First-pass framework to evolve programs

--- Turwin/MainClass.cs
+++ Turwin/MainClass.cs
@@ -22,7 +22,12 @@ namespace Turwin
                 /// <summary>
                 /// The maximum number of loop iterations for the interpreter.
                 /// </summary>
-                private const int maxiterations = 1;
+                private const int maxiterations = 1000;
+
+                /// <summary>
+                /// The maximum number of generations.
+                /// </summary>
+                private const int maxgenerations = 100;
 
                 /// <summary>
                 /// The entry point of the program, where the program control starts and ends.
@@ -30,12 +35,12 @@ namespace Turwin
                 /// <param name="args">The command-line arguments.</param>
                 public static void Main(string[] args)
                 {
+                        var rng = new Random();
                         var stdin = new List<int>() { 5 };
                         var stdout = new List<int>() { 6 };
                         var fit = new List<FitnessSelector>();
-                        string nl = Environment.NewLine;
-                        string genometext = string.Empty;
                         double threshold = double.Epsilon;
+                        int generation = 0;
 
                         if (args.Length == 0)
                         {
@@ -47,36 +52,63 @@ namespace Turwin
                         var grammartext = input.ReadToEnd();
                         input.Close();
 
-                        if (args.Length > 1)
+                        do
                         {
-                                input = File.OpenText(args[1]);
-                                genometext = input.ReadToEnd();
-                                input.Close();
-                        }
+                                EvaluateNextGeneration(stdin, stdout, fit, threshold, grammartext);
 
-                        for (int i = 0; i < MainClass.population; i++)
+                                int parents = fit.Count;
+                                for (int i = parents; i < MainClass.population - 1000; i++)
+                                {
+                                        int mother = rng.Next(parents);
+                                        int father = rng.Next(parents);
+                                        FitnessSelector child = fit[mother].Mate(fit[father]);
+                                        fit.Add(child);
+                                }
+
+                                threshold = fit[parents - 1].Rating;
+                                generation += 1;
+                        }
+                        while (generation < MainClass.maxgenerations && fit.Count >= 1 && fit[0].Rating < 100);
+
+                        Console.WriteLine("# Best program");
+                        Console.WriteLine(fit[0].Program);
+                }
+
+                /// <summary>
+                /// Evaluates the next generation.
+                /// </summary>
+                /// <param name="stdin">Input values.</param>
+                /// <param name="stdout">Expected output values.</param>
+                /// <param name="programs">List of programs.</param>
+                /// <param name="threshold">Threshold rating.</param>
+                /// <param name="grammartext">Grammar text.</param>
+                static void EvaluateNextGeneration(List<int> stdin, List<int> stdout, List<FitnessSelector> programs, double threshold, string grammartext)
+                {
+                        for (int i = programs.Count; i < MainClass.population; i++)
                         {
-                                var f = new FitnessSelector(grammartext, genometext, stdin, stdout);
-                                genometext = string.Empty;
+                                var f = new FitnessSelector(grammartext, string.Empty, stdin, stdout);
                                 f.Execute(maxiterations);
                                 f.Evaluate(threshold);
-                                fit.Add(f);
-                                bool accept = f.Rating >= threshold;
-                                Console.WriteLine(i.ToString() + " - " + accept.ToString() + ": " + f.Rating.ToString() + "%");
+                                programs.Add(f);
+                                // bool accept = f.Rating >= threshold;
+                                // Console.WriteLine(i.ToString() + " - " + accept.ToString() + ": " + f.Rating.ToString() + "%");
                         }
 
-                        fit.Sort();
-                        for (int i = 0; i < fit.Count; i++)
+                        programs.Sort();
+                        for (int i = 0; i < programs.Count; i++)
                         {
-                                if (fit[i].Rating < threshold)
+                                if (programs[i].Rating < threshold)
                                 {
-                                        fit.RemoveRange(i, fit.Count - i);
+                                        programs.RemoveRange(i, programs.Count - i);
                                         break;
                                 }
                         }
 
-                        Console.WriteLine("Number of runs that exceed the threshold of " + threshold.ToString()
-                                + ":  " + fit.Count.ToString());
+                        Console.WriteLine("#Runs over threshold of " + threshold.ToString() + "%: "
+                                + programs.Count.ToString()
+                                + "; best rating is "
+                                + programs[0].Rating
+                                + "%");
                 }
         }
 }
